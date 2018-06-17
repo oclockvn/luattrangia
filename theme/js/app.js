33c299bdef8950a757775912bf776c5a149a26b4
oclockvn <oclockvn@gmail.com>
2018-06-18 03:00:47 +0700

    added latest posts

--- theme/js/app.js
+++ theme/js/app.js
@@ -1,9 +1,11 @@
 class News {
-    constructor(title, url, createDate, category) {
+    constructor(title, url, createDate, category, creatorName, excerpt) {
         this.title = title;
         this.url = url;
         this.createDate = createDate;
         this.category = category;
+        this.creatorName = creatorName;
+        this.excerpt = excerpt;
     }
 }
 
@@ -15,19 +17,44 @@ class QuickNewsViewModel {
                 title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
                 url: "http://lorem-ipsum-dolor-sit-amet.com",
                 category: "video",
-                createDate: "2 days ago"
+                createDate: "2 days ago",
+                creatorName: "admin",
+                excerpt: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit eos nobis voluptates laboriosam ex ducimus inventore dolor obcaecati voluptatum ut nostrum unde molestias, incidunt non qui! Facere dolor excepturi tenetur."
             },
             {
                 title: "Sit eos nobis voluptates laboriosam ex ducimus inventore",
                 url: "http://lorem-ipsum-dolor-sit-amet.com",
                 category: "design",
-                createDate: "5 minutes ago"
+                createDate: "5 minutes ago",
+                creatorName: "admin",
+                excerpt: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit eos nobis voluptates laboriosam ex ducimus inventore dolor obcaecati voluptatum ut nostrum unde molestias, incidunt non qui! Facere dolor excepturi tenetur."
             },
             {
                 title: "incidunt non qui! Facere dolor excepturi tenetur",
                 url: "http://lorem-ipsum-dolor-sit-amet.com",
                 category: "business",
-                createDate: "3 days ago"
+                createDate: "3 days ago",
+                creatorName: "admin",
+                excerpt: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit eos nobis voluptates laboriosam ex ducimus inventore dolor obcaecati voluptatum ut nostrum unde molestias, incidunt non qui! Facere dolor excepturi tenetur."
+            }
+        ]);
+
+        this.posts = ko.observableArray([
+            {
+                title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+                url: "http://lorem-ipsum-dolor-sit-amet.com",
+                category: "video",
+                createDate: "2 days ago",
+                creatorName: "admin",
+                excerpt: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit eos nobis voluptates laboriosam ex ducimus inventore dolor obcaecati voluptatum ut nostrum unde molestias, incidunt non qui! Facere dolor excepturi tenetur."
+            },
+            {
+                title: "Sit eos nobis voluptates laboriosam ex ducimus inventore",
+                url: "http://lorem-ipsum-dolor-sit-amet.com",
+                category: "design",
+                createDate: "5 minutes ago",
+                creatorName: "admin",
+                excerpt: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit eos nobis voluptates laboriosam ex ducimus inventore dolor obcaecati voluptatum ut nostrum unde molestias, incidunt non qui! Facere dolor excepturi tenetur."
             }
         ]);
     }    
