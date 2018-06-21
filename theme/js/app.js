14064fb087a6b0dca674c8f239bb4108b93f0076
oclockvn <oclockvn@gmail.com>
2018-06-21 23:56:35 +0700

    added category

--- theme/js/app.js
+++ theme/js/app.js
@@ -58,20 +58,46 @@ class QuickNewsViewModel {
             }
         ]);
 
-        this.faq = ko.observableArray([1,2,3,4,5,6]);
-        this.ads = ko.observableArray([1,2]);
-        this.clients = ko.observableArray([1,2,3,4,5,6]);
+        this.faq = ko.observableArray([1, 2, 3, 4, 5, 6]);
+        this.ads = ko.observableArray([1, 2]);
+        this.clients = ko.observableArray([1, 2, 3, 4, 5, 6]);
         this.tags = ko.observableArray([
-            'Lorem ipsum', 'dolor', 'sit amet', 
-            'consectetur adipisicing', 'elit', 
-            'Laudantium aliquid', 'veritatis voluptatibus', 
-            'culpa illo quae', 'illum optio', 
-            'ducimus minus ut', 'quaerat excepturi', 
-            'quibusdam', 'at ratione repellendus', 
+            'Lorem ipsum', 'dolor', 'sit amet',
+            'consectetur adipisicing', 'elit',
+            'Laudantium aliquid', 'veritatis voluptatibus',
+            'culpa illo quae', 'illum optio',
+            'ducimus minus ut', 'quaerat excepturi',
+            'quibusdam', 'at ratione repellendus',
             'perspiciatis', 'impedit explicabo quasi!'
         ]);
-        this.services = ko.observableArray([1,2,3,4,5,6]);
-    }    
+        this.services = ko.observableArray([1, 2, 3, 4, 5, 6]);
+        this.submenu = ko.observableArray([{
+            "n": "Mfnt baowu qb lyrtv cbwtb"
+        }, {
+            "n": "Xdnq dbjmk ay sawzk jfdba"
+        }, {
+            "n": "Alvn ryzeu gt qnusp psctj"
+        }, {
+            "n": "Ntvt cxvfn fp uxlnw qonko"
+        }, {
+            "n": "Mxtd uhhwt uo rttci mjxaa"
+        }]);
+        this.mostreadnews = ko.observableArray([{
+            "n": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue."
+        }, {
+            "n": "Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend."
+        }, {
+            "n": "Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."
+        }, {
+            "n": "Aliquam non mauris. Morbi non lectus."
+        }, {
+            "n": "Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl."
+        }, {
+            "n": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor."
+        }, {
+            "n": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+        }]);
+    }
 }
 
 ko.applyBindings(new QuickNewsViewModel());
